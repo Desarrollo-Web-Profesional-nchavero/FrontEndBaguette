e267a6b8f9a05b644de365f1d8d621ac013edd79
SekiroK5 <arin01246@gmail.com>
2026-03-12 17:33:23 -0600

    listo para produccion

--- src/servicios/api.js
+++ src/servicios/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: "https://backendbaguette-production-ca7f.up.railway.app/api/v1"
 });
 
 
